a3c3ee06f740528a9d22652e62f43bee6491020b
Marcus Gutierrez <marcusgttrz@gmail.com>
2024-04-20 09:43:46 -0600

    init

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import boto3
+import uuid
 
 max_deployments = 5
 bucket_name = "s3-bucket-name"
 
 endpoint_url = "http://localstack:4566"
+
+def init():
+    client = boto3.client("s3", endpoint_url=endpoint_url)
+    client.create_bucket(Bucket=bucket_name)
+
+    for x in range(10):
+        # Generate x directories/objects with a random string/hash
+        client.put_object(Bucket=bucket_name, Key=f"{uuid.uuid4().hex[:6]}/index.html")
 
 
 def main():
@@ -13,4 +22,5 @@ def main():
 
 
 if __name__ == '__main__':
+    init()
     main()
